19a80f20e2a13c179cce1c9f947634fc13a36792
genatol31 <genatol26@gmail.com>
2021-03-08 19:15:41 +0200

    Homework number 5

---
+++ home_work_5.py
@@ -0,0 +1,43 @@
+import os
+# name = input()
+# def serch(n):
+#     some_dict = {}
+#     cont = 0
+#     if n in os.listdir():
+#         for i in os.listdir(n):
+#             a1 = i.split('.')
+#             a2 = a1[1]
+#             if a2 in i:
+#                 cont = cont + 1
+#             a = {a2: cont}
+#             some_dict.update(a)
+#         print(some_dict)
+#
+#     else:
+#         print('No')
+# serch(name)
+
+# way = input()
+# name = input()
+#
+# def coint_file (n, m):
+#     file_list = []
+#     for i in os.listdir(n):
+#         a = i.split('.')
+#
+#         if a[1] == m:
+#             file_list.append(i)
+#     print(file_list)
+# coint_file(way, name)
+
+# way = input()
+# def func(n):
+#
+#     for i in os.listdir(n):
+#         a = i.split('.')[-1]
+#         if a not in os.listdir(n):
+#             os.mkdir(n + a)
+#         elif a in i:
+#             os.replace(n + '/' + i, n + '/' + a + '/' + i)
+#
+# func(way)
