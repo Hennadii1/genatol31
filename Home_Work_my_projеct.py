80c205ba8e4c561851ed55af48908ff65f537b10
genatol31 <genatol26@gmail.com>
2021-04-18 20:28:12 +0300

    filter

--- Home_Work_my_projеct.py
+++ Home_Work_my_projеct.py
@@ -1,4 +1,17 @@
+"""
+Программа фильтрации показаний тензометрического датчика.
+В файде записана построчно функция y(x) в ыиде "значение Y / значение х"
+Необходимо считать с файла данные и записать в отдельные массивы значения x и y.
+Затем сделать преобразования с значениями и записать результат в файл.
+
+"""
+
+
+
 class InputData:
+    """
+    Определяем глобальный класс данных
+    """
     def __init__(self, data):
         self.data = list(data)
 
@@ -6,46 +19,61 @@ class InputData:
 
     @staticmethod
     def input_data_file():
+        """
+        Импортируем данные из файла и записываем в массив
+        """
         data = []
         file_name = 'input_data'
         with open(file_name) as f:
-           for i in f:
-               data.append(i)
-        return data
+            for i in f:
+                data.append(i)
+            yield data
+
 
     def tranformation_data(self):
+        """
+        Разделяем данные на два массива
+        """
         x = []
         y = []
-        for i in InputData.input_data_file():
-            data = i.split('/')
-            x.append(float(data[1]))
-            y.append(float(data[0]))
+        for i in self.data:
+            for j in i:
+                data = j.split('/')
+                x.append(float(data[1]))
+                y.append(float(data[0]))
         return y, x
 
 
 class FilterData(InputData):
-    def tranformation_data(self):
+    """
+    Определяем дочерний класс преобразований над данными
+    """
+    def tranformation_data_new(self):
+        """
+        Фильтруем значения функции
+        """
         filter_x = []
-        for i in range(len(self.tranformation_data()[0])-1):
-            item = (self.tranformation_data()[0][i] + self.tranformation_data()[0][i+1])/2
+        for i in range(len(self.tranformation_data()[0])):
+            item = (self.tranformation_data()[0][i] + self.tranformation_data()[0][i])/2
             filter_x.append(item)
         return filter_x
 
     def data_output(self):
+        """
+        Записываем результаты в файл
+        """
         file_name = 'data_output'
         with open(file_name, 'a') as f:
             f.write('X' + '\t\t' + 'Y' + '\n\n')
-            f.write(str(self.tranformation_data()[0][0]) + '\t\t' + str(self.tranformation_data()[1][0]) + '\n')
-            for i in range(len(self.tranformation_data()[0])-1):
-                f.write(str(self.tranformation_data()[0][i]) + '\t\t' + str(self.tranformation_data()[1][i+1]) + '\n')
-
-
+            for i in range(len(self.tranformation_data()[0])):
+                f.write(str(self.tranformation_data()[0][i]) + '\t\t' + str(self.tranformation_data()[1][i]) + '\n')
 
 
 object1 = InputData(InputData.input_data_file())
-filter1 = FilterData
-filter1.tranformation_data(object1)
-filter1.data_output(object1)
+object1.tranformation_data()
 
-# print(object1.tranformation_data()[0])
-# print(object1.tranformation_data()[1])
+
+FilterData.tranformation_data_new(object1)
+FilterData.data_output(object1)
+
+
