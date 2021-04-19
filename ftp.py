3cca17b540627da2547d3a63b1e2a3665745cfd7
user <genatol26@gmail.com>
2021-04-19 20:48:55 +0300

    ftp

---
+++ ftp.py
@@ -0,0 +1,45 @@
+from ftplib import FTP
+# ftp = FTP('ftp.us.debian.org')
+# ftp.login()
+#
+# ftp.cwd('debian')
+# data = ftp.nlst()
+# print(data)
+# ftp.quit()
+
+class FTPdownload:
+    def __init__(self, adress):
+        self.adress = adress
+        self.ftp = FTP(self.adress)
+        self.ftp.login()
+
+
+
+    def direcrory(self, dicectory):
+        self.direcrory = dicectory
+        self.ftp.cwd(dicectory)
+        data = self.ftp.nlst()
+        return data
+
+    def filter_file(self,files):
+        data_file = []
+        for i in files:
+            if i.count('.'):
+                data_file.append(i)
+        return data_file
+
+    def download(self, data):
+
+        for i in data:
+            with open(i, 'wb') as fp:
+                self.ftp.retrbinary('RETR ' + i, fp.write)
+        self.ftp.quit()
+
+
+adress = FTPdownload('ftp.us.debian.org')
+n = adress.direcrory('debian')
+
+print(n)
+m = adress.filter_file(n)
+print(m)
+adress.download(m)
